f7acd526084139e71c2429f170e9a69bb6efb79e
revesansparole <revesansparole@gmail.com>
2016-08-02 16:42:38 +0200

    better handling of binary data

--- src/see_example/init_container.py
+++ src/see_example/init_container.py
@@ -1,4 +1,3 @@
-from base64 import b64encode
 from see_scripts.see_client import (connect, get_ro_def, get_single_by_name,
                                     log_to_see, register_ro, remove_ro)
 
@@ -14,7 +13,11 @@ except KeyError:
 
 cid = register_ro(session,
                   'container',
-                  dict(name=container, id="6721cbfc526f11e6b255d4bed973e64a"))
+                  dict(id="6721cbfc526f11e6b255d4bed973e64a",
+                       name=container,
+                       ctype="project",
+                       description="Example project to show the capabilities "
+                                   "of the SEE platform"))
 
 
 # upload image on SEEweb
@@ -23,9 +26,11 @@ roid = '4f3a11b6528311e6b255d4bed973e64a'
 if get_ro_def(roid, session) is not None:
     remove_ro(session, roid, False)
 
-img = dict(id=roid, name="lena")
+img = dict(id=roid,
+           name="lena",
+           description="Classical sample image for demo purpose")
 with open("lena.png", 'rb') as f:
-    img['value'] = b64encode(f.read())
+    img['value'] = f.read()
 
 pid = register_ro(session, 'image', img)
 connect(session, cid, pid, 'contains')
